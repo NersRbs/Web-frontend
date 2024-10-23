4227d764770f28404a356522d843e12ef2da67f2
Artem <artemgun2004@gmail.com>
2024-10-23 17:39:54 +0300

    lab4 done

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+function displayLoadTime() {
+    let loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+    let footer = document.querySelector('footer');
+    let loadInfo = document.createElement('p');
+
+    loadInfo.textContent = `${loadTime} мс`;
+    loadInfo.style.color = 'lightgray';
+    loadInfo.style.fontSize = '10px';
+    loadInfo.style.margin = '0';
+    loadInfo.style.textAlign = 'right';
+
+    footer.appendChild(loadInfo);
+}
+
+function handleNavLinks() {
+    const navLinks = document.querySelectorAll('.nav-menu a');
+    let currentPath = window.location.pathname;
+
+    if (currentPath === '/' || currentPath === '/index.html') {
+        currentPath = '/index.html';
+    }
+
+    navLinks.forEach(link => {
+        let linkPath = new URL(link.href).pathname;
+
+        if (linkPath === '/') {
+            linkPath = '/index.html';
+        }
+
+        if (linkPath === currentPath) {
+            link.classList.add('active');
+        }
+    });
+}
+
+(function() {
+    window.addEventListener('load', function() {
+        displayLoadTime();
+        handleNavLinks();
+    });
+})();
