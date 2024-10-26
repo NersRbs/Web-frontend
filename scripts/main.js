5b98140fbc4dcb12881ba61fb59051e77af30012
Artem <artemgun2004@gmail.com>
2024-10-26 15:04:44 +0300

    refactor

--- scripts/main.js
+++ scripts/main.js
@@ -16,8 +16,8 @@ function handleNavLinks() {
     const navLinks = document.querySelectorAll('nav a');
     let currentPath = window.location.pathname;
 
-    if (currentPath === '/' || currentPath === '/index.html') {
-        currentPath = '/index.html';
+    if (currentPath === '/Web-frontend/') {
+        currentPath = '/Web-frontend/index.html';
     }
 
     navLinks.forEach(link => {
