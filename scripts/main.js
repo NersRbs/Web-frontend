bd1683a27345c5c9773ba152a1cf7d3df59a59bf
Artem <artemgun2004@gmail.com>
2024-11-22 14:25:46 +0300

    refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,13 @@
-function displayLoadTime() {
-    let loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-    let footer = document.querySelector('footer');
-    let loadInfo = document.createElement('p');
-
-    loadInfo.textContent = `${loadTime} мс`;
-    loadInfo.style.color = 'lightgray';
-    loadInfo.style.fontSize = '10px';
-    loadInfo.style.margin = '0';
-    loadInfo.style.textAlign = 'right';
-
-    footer.appendChild(loadInfo);
-}
-
 function handleNavLinks() {
     const navLinks = document.querySelectorAll('nav a');
     let currentPath = window.location.pathname;
 
-    if (currentPath === '/Web-frontend/') {
-        currentPath = '/Web-frontend/index.html';
+    if (currentPath === "/Resume/") {
+        currentPath = '/Resume/index.html';
     }
 
     navLinks.forEach(link => {
         let linkPath = new URL(link.href).pathname;
-
-        if (linkPath === '/') {
-            linkPath = '/index.html';
-        }
 
         if (linkPath === currentPath) {
             link.classList.add('active');
@@ -33,10 +15,8 @@ function handleNavLinks() {
     });
 }
 
-
 (function() {
     window.addEventListener('load', function() {
-        displayLoadTime();
         handleNavLinks();
     });
 })();
